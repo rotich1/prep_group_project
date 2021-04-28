f740c137e475a3369b08375b527040d8c4d3d80d
Softonic254 <rotichenoch27@gmail.com>
2021-04-28 09:18:31 +0300

    It will restore html

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("img.money").hide();
+    $("button.start").mouseover(function(){
+        $("img.money").toggle(2000);
+
+    });
+
+    // $("button.start").mouseover(function () {
+    //     $("image.money").toggle(2000);
+
+    // });
+});
