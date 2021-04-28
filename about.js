f652b845e9405894a4f92503859355ad3f52a924
Kibet 1998 <ronoronald1998@gmail.com>
2021-04-28 17:54:10 +0300

    add motto

---
+++ about.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+    $("#kiptooimage").click(function(){
+        $("#kiptoo").toggle()
+
+        
+    })
+
+    $("#kiptoo").click(function(){
+        $("#kiptoo").hide()
+
+        
+    })
+
+    $("#ronaldimage").click(function(){
+        $("#ronaldp").toggle()
+
+        
+    })
+
+    $("#ronald").click(function(){
+        $("#ronaldp").hide()
+
+        
+    })
+
+    $("#muriithi").click(function(){
+        $("#muriithip").toggle()
+
+        
+    })
+
+    $("#muriithi").click(function(){
+        $(".muriithip").hide()
+
+        
+    })
+
+
+    $("#victor").click(function(){
+        $("#victorp").toggle()
+
+        
+    })
+
+    $("#victor").click(function(){
+        $(".victorp").hide()
+
+        
+    })
+
+
+    
+    
+})
