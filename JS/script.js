1d547a65b55675a4975d262c1a39fe3da6c6222d
Softonic254 <rotichenoch27@gmail.com>
2021-04-28 09:22:41 +0300

    It will add jQuery function

--- JS/script.js
+++ JS/script.js
@@ -2,11 +2,8 @@ $(document).ready(function(){
     $("img.money").hide();
     $("button.start").mouseover(function(){
         $("img.money").toggle(2000);
+        $("img.money").toggle(2000);
 
     });
 
-    // $("button.start").mouseover(function () {
-    //     $("image.money").toggle(2000);
-
-    // });
 });
